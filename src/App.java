ffaf35037f037d95889d9b9f9b798d394103d67d
Flávio Paixão <flavio_pc@outlook.com>
2022-06-28 10:52:11 -0300

    criação do strategy

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.Comportamento;
+import strategy.ComportamentoAgressivo;
+import strategy.ComportamentoDefensivo;
+import strategy.ComportamentoNormal;
+import strategy.Robo;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SingletonLazy singletonLazy = SingletonLazy.getInstancia();
+        System.out.println(singletonLazy);
+        singletonLazy = SingletonLazy.getInstancia();
+        System.out.println(singletonLazy);
+
+        SingletonEager singletonEager = SingletonEager.getInstancia();
+        System.out.println(singletonEager);
+        singletonEager = SingletonEager.getInstancia();
+        System.out.println(singletonEager);
+
+        SingletonLazyHolder singletonLazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(singletonLazyHolder);
+        singletonLazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(singletonLazyHolder);
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
     }
 }
